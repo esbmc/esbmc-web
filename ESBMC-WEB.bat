6c1e6d5ddc6614fb79b23d9dce744617ce0feb45
Silvano Tavares Batista Júnior <silvano.junior@ufam.edu.br>
2026-06-09 11:54:01 -0400

    Update ESBMC-WEB.bat

--- ESBMC-WEB.bat
+++ ESBMC-WEB.bat
@@ -1,21 +1,22 @@
 @echo off
 title Inicializador ESBMC-Web
 echo ==========================================
-echo Iniciando o ESBMC-Web...
+echo A iniciar o ESBMC-Web...
 echo ==========================================
 
-:: Garante que o terminal do Windows reconheça a pasta atual
+:: Garante que o Windows reconhece a pasta atual
 cd /d "%~dp0"
 
-:: Corrige quebras de linha invisíveis do Windows no script de inicialização Linux (CRLF para LF)
+:: Limpa quebras de linha do Windows (CRLF para LF) que causam bugs no Linux
 wsl -e bash -c "sed -i 's/\r$//' runner.sh"
+wsl -e bash -c "sed -i 's/\r$//' backend/requirements.txt"
 
-:: Executa o runner.sh de forma isolada dentro do WSL
+:: Executa o script do Linux e mantém a janela aberta
 start "Backend ESBMC-Web" cmd /k "wsl -e bash runner.sh"
 
-echo Aguardando o servidor subir...
+echo A aguardar que o servidor inicie...
 timeout /t 8 /nobreak > nul
 
-:: Abre a interface gráfica no navegador padrão
+:: Abre o navegador
 start "" "frontend\index.html"
 exit
